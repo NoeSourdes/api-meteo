36f625b3eb3d621d2366ddb8f22428740c431a57
Noé Sourdès <souredesnoe19@gmail.com>
2022-11-06 14:06:17 +0100

    finalisation index.js api-meteo

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-console.log("hello world")
+const prompts = require('prompts')
+async function main(){
+
+/*
+const settings = {
+    accessKey: "b804d327facaacf4b439b53e2e2641e3",
+    location: "Paris",
+    units: "m"
+}
+fetch(`http://api.weatherstack.com/current?access_key=${settings.accessKey}&query=${settings.location}&units=${settings.units}`)
+    .then(res => res.json())
+    .then(data => {
+        const {current, location, requets} = data
+        console.log(`La temperature à ${location.name}°C, est de ${current.temperature}°C et le resenti est de ${current.feelslike}°C`)
+    })
+*/
+let unit
+const locationVille = await getLocationVille();
+const unitDegFar = await getUnitDegFar();
+if(unitDegFar === "°C"){
+    unit = "m"
+}
+if(unitDegFar === "°F"){
+    unit = "f"
+}
+const weather = (location, unit, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=b804d327facaacf4b439b53e2e2641e3&query=${location}&units=${unit}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                if(data.success === false){
+                    callback(`Impossible de renvoyer vos informations. Erreur ${data.err.code}: ${data.err.info}`, undefined)
+                }
+                else{
+                    const {location, current} = data
+                    callback(undefined,`La temperature à ${location.name}, est de ${current.temperature}°C et le resenti est de ${current.feelslike}°C`)
+                }
+            })
+}
+weather(locationVille, unit, (err, data) => {
+    console.log('Erreur :', err)
+    console.log('Data :', data)
+})
+}
+async function getLocationVille(){
+    const location = await prompts({
+        type: "text",
+        name: "value",
+        message: "Donne moi une ville en respectant les majuscules ?"
+    })
+    return location.value
+}
+async function getUnitDegFar(){
+    const unit = await prompts({
+        type: "text",
+        name: "value",
+        message: "Donne moi l'unité : °C, °F",
+    })
+    return unit.value
+}
+main();
